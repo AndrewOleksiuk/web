7bf97aa1fc1397b75d79bae09e008bc3f635fe26
Andriy Oleksyuk <andre200129@gmail.com>
2020-10-07 23:50:19 +0300

    Add animations to elements

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,59 @@ $(document).on("scroll", function () {
     else
       $("header").attr("class", "fixed");
   });
-  
+
+  var isScrolling = false;
+  window.addEventListener("scroll", throttleScroll, false);
+
+  function throttleScroll(e) {
+    if (isScrolling == false) {
+      window.requestAnimationFrame(function() {
+        scrolling(e);
+        isScrolling = false;
+      });
+    }
+    isScrolling = true;
+  }
+
+  document.addEventListener("DOMContentLoaded", scrolling, false);
+  var firstBox = document.querySelector("#main");
+  var secondBox = document.querySelector("#team");
+  var thirdBox = document.querySelector("#footer");
+
+  function scrolling(e) {
+
+    if (isPartiallyVisible(firstBox)) {
+      firstBox.classList.add("active");
+    } else {
+      firstBox.classList.remove("active");
+    }
+    if (isFullyVisible(secondBox)) {
+      secondBox.classList.add("active");
+    } else {
+      secondBox.classList.remove("active");
+    }
+    if (isPartiallyVisible(thirdBox)) {
+      thirdBox.classList.add("active");
+    } else {
+      thirdBox.classList.remove("active");
+    }
+  }
+
+  function isPartiallyVisible(el) {
+    var elementBoundary = el.getBoundingClientRect();
+
+    var top = elementBoundary.top;
+    var bottom = elementBoundary.bottom;
+    var height = elementBoundary.height;
+
+    return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+  }
+
+  function isFullyVisible(el) {
+    var elementBoundary = el.getBoundingClientRect();
+
+    var top = elementBoundary.top;
+    var bottom = elementBoundary.bottom;
+
+    return ((top >= 0) && (bottom <= window.innerHeight));
+  }
